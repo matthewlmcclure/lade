58a11c3a04df5f433660dcf021a82f4711ca4d73
Matt McClure <identity@matthewlmcclure.com>
2016-07-19 20:58:48 -0400

    Only export interfaces known to be required

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 require('coffee-script/register');
 
 module.exports = {
-  PACKAGE_INFO: require('./package.json'),
   CLI:          require('./lib/cli'),
-  LANGUAGES:    require('./lib/languages'),
-  Project:      require('./lib/project'),
-  Renderer:      require('./lib/renderer')
 };
