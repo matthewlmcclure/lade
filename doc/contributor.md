6e3d4d65cd8db7da8fcd9264af99be517d9a3f38
Matt McClure <identity@matthewlmcclure.com>
2016-07-19 20:58:32 -0400

    Document `index.js`

--- doc/contributor.md
+++ doc/contributor.md
@@ -4,6 +4,7 @@ target: contributor/index.html.md
 ---
 includes:
   - contributor/internals/bin/groc.md
+  - contributor/index.md
   - contributor/lib/cli.md
   - contributor/lib/project.md
   - contributor/lib/project/constructor.md
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// ---
+// target: includes/contributor/_index.md
+// ---
+// ## Module Exports
+//
+// The `groc` module exports the `CLI` interface.
+
+// Register the CoffeeScript compiler
 require('coffee-script/register');
 
+// Declare the interfaces exported
 module.exports = {
   CLI:          require('./lib/cli'),
 };
